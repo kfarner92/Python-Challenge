d44d47759641b45e58879d9b085d6bc7b5f35ee5
KFarner <37426030+kfarner92@users.noreply.github.com>
2018-06-24 22:03:20 -0500

    updating script

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,25 +3,19 @@ import csv
 #Source filepath
 csvpath = os.path.join('..','PyBank','budget_data.csv')
 # Storage Lists
-date = []
-revenue = []
 
-with open(csvpath,newline='')as csvfile:
+totalMonths = 0
+netProfitLoss = 0
+largestAvgChange = 0.00
+largestProfit = 0
+largestLoss = 0
+
+with open(csvpath,newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
+    header = next(csvreader)
+
     for row in csvreader:
-        date.append(row[0])
-        revenue.append(row[1])
-cleaned_csv = zip(date,revenue)
-output_file = os.path.join('..','PyBank','PyBank_Final.csv')
-with open(output_file, 'W', newline="") as datafile:
-    csvwriter = csv.writer(datafile)
-    csvwriter.writerow(["date","revenue"])
-    csvwriter.writerows(cleaned_csv)
-
-#    print(csvreader)
-
-#   csv_header = next(csvreader)ß
-#   print(f"CSV Header: {csv_header}")
-#   for row in csvreader:
-#       print (row)
+ #       totalMonths = totalMonths + int(count(row[0]))
+        netProfitLoss = netProfitLoss + int(row[1])
+#print(netProfitLoss)
