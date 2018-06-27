f26b13933629f861fa1743ffe69685f17ee79999
KFarner <37426030+kfarner92@users.noreply.github.com>
2018-06-26 22:15:40 -0500

    finish part one

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,20 +2,53 @@ import os
 import csv
 #Source filepath
 csvpath = os.path.join('..','PyBank','budget_data.csv')
-# Storage Lists
-
-totalMonths = 0
+# Storage Lists and variables
 netProfitLoss = 0
-largestAvgChange = 0.00
-largestProfit = 0
-largestLoss = 0
-
+row_count = 0
+months = []
+revenue = []
+#open and read csv
 with open(csvpath,newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     header = next(csvreader)
 
     for row in csvreader:
- #       totalMonths = totalMonths + int(count(row[0]))
         netProfitLoss = netProfitLoss + int(row[1])
+        row_count = row_count + 1
+        months.append(row[0])
+        revenue.append(int(row[1]))
+#storing greatest increase and decrease values
+moneyWaterfall = revenue[0]
+burningBenjamins = revenue[0]
+for row in range(len(revenue)):
+    if revenue[row] >= moneyWaterfall:
+        moneyWaterfall = revenue[row]
+        bestMonth = months[row]
+    elif revenue[row] <= burningBenjamins:
+        burningBenjamins = revenue[row]
+        worstMonth = months[row]
+#getting the average revenue amount
+averageRevenue = round(netProfitLoss/row_count)
+
+outputLocation = os.path.join('../Pybank/Pybank_Final.txt')
+with open(outputLocation,'w') as writeFile:
+    writeFile.writelines('Financial Analysis\n')
+    writeFile.writelines('------------------------------'+ '\n')
+    writeFile.writelines('Total Months: '+str(row_count) + '\n')
+    writeFile.writelines('Total Profit/Loss: $' +str(netProfitLoss) + '\n')
+    writeFile.writelines('Average Change: $' + str(averageRevenue) + '\n')
+    writeFile.writelines('Greatest Increase in Profits: '+str(bestMonth) +' ($'+str(moneyWaterfall)+')' + '\n')
+    writeFile.writelines('Greatest Decrease in Profits: '+str(worstMonth) +' ($'+str(burningBenjamins)+')' + '\n')
+
+with open(outputLocation, 'r') as readFile:
+    print (readFile.read())
+#testing
+#print(averageRevenue)
 #print(netProfitLoss)
+#print(row_count)
+#print(burningBenjamins)
+#print(worstMonth)
+#print(moneyWaterfall)
+#print(bestMonth)
+# row_count = sum(1 for row in csvreader)
